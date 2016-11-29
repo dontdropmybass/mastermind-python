c838ef10bbd50cd658e5a63814208ad99e6085aa
Alexander Cochrane <acochrane@almightycorp.com>
2016-11-29 11:41:56 -0400

    replace mastermind with mastermind2

--- MasterMind.py
+++ MasterMind.py
@@ -1,28 +1,87 @@
-__author__ = 'benvc'
 import random
-Turn = 1
-CorrectArray = ["","","",""]
-Array0 = ['#',"#","#","#",""]
-Array1 = ["x","x","x","x",""]
-Array2 = ["x","x","x","x",""]
-Array3 = ["x","x","x","x",""]
-Array4 = ["x","x","x","x",""]
-Array5 = ["x","x","x","x",""]
-Array6 = ["x","x","x","x",""]
-Array7 = ["x","x","x","x",""]
-Array8 = ["x","x","x","x",""]
-ArrayList = [Array0,Array1,Array2,Array3,Array4,Array5,Array6,Array7,Array8]
-##Print List Method
-def printList():
-    for i in range(9):
-        print(ArrayList[i][0], ArrayList[i][1],ArrayList[i][2],ArrayList[i][3],ArrayList[i][4])
-def CheckCorrect():
-    rand = 0
-#Random Number for Correct list
-for i in range(4):
-    rand = random.randrange(10)
-    CorrectArray[i] = (rand)
+import os
+import time
+import MastermindDBConn as db
 
-print("Welcome to MasterMind, try to crack the within 8 tries to win.")
-printList()
+def clear():
+    # this doesn't work
+    try:
+        return os.system("cls") + os.system("clear")
+    except:
+        return 1
+# code as an array
+code = []
+# guess counter
+guess = 0
+# whether or not the game is still being played
+game = True
 
+def genCode():
+    # generated a random code
+    code = [random.randrange(10),random.randrange(10),random.randrange(10),random.randrange(10)]
+    return code
+
+def doGuess(guess):
+    inCode = 0
+    correctPos = 0
+    # user makes a guess
+    currentGuess = input("Enter your guess #"+str(guess)+": ")
+    currentGuess.strip()
+    b = True
+    # check if the guess is in the correct format
+    if len(currentGuess) != 4:
+        b = False
+    for i in range(len(currentGuess)):
+        if not currentGuess[i].isdigit():
+            b = False
+    if not b:
+        # show them they're wrong and try again
+        input("Guesses must be 4 numbers in length. Press enter to continue...")
+        currentGuess = ""
+        clear()
+        doGuess(guess)
+    for i in range(len(currentGuess)):
+        # check the code and show them how many
+        # numbers they guessed correctly
+        if int(currentGuess[i]) == code[i]:
+            correctPos += 1
+        if int(currentGuess[i]) in code:
+            inCode += 1
+    print("You guessed "+str(inCode)+" numbers in the code, and "+str(correctPos)+" in the correct position.")
+    if correctPos == 4:
+        # if they guess the code correctly,
+        # end the game
+        return False
+    else:
+        return True
+
+##### Main Game Code
+# ask for their name
+name = input("Welcome to mastermind, please enter your name to start: ")
+# generate the code
+code = genCode()
+# find out what time it is right now
+start = time.time()
+# for ten guesses,
+while guess < 10 and game:
+    clear()
+    guess += 1
+    # do the guess
+    game = doGuess(guess)
+
+if not game:
+    # if they win, put their score in the database
+    print("You won in "+str(guess)+" turns! Good job!\n\n")
+    print("--------------------------------------------------\n\n")
+    db.Insert(name,str(guess),str(time.time()-start))
+    db.Select()
+else:
+    # if they don't win, tell them the code
+    # and how bad they are
+    print("Sorry, you fail.")
+    print("The correct combo is: ")
+    print(code)
+    print("\n\n--------------------------------------------------\n\n")
+    db.Select()
+input("\n\nPress enter to quit")
+db.Close()
