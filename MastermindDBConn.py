ef01673d292ca5b4cd5a8b114966012b9ed54e11
BenCovey <benvcovey@gmail.com>
2016-11-29 11:04:46 -0400

    Update MastermindDBConn.py

--- MastermindDBConn.py
+++ MastermindDBConn.py
@@ -1,11 +1,31 @@
 #!/usr/bin/python
 import sqlite3
+
+def Open():
+    conn= sqlite3.connect('HighScores.sqlite')
+
+def Select():
+    #Select Statement
+    cursor = conn.execute("SELECT * from Scores ORDER BY Stage, Time LIMIT 0,10")
+    for row in cursor:
+        print("Name: " , row[1], " Stage: " , row[2], " Time: " , row[3])
+        
+def Insert():
+    #Gather values
+    name = input("Enter Name: ")
+    Stage = input("Stage: ")
+    Time = input("Time: ")
+    #Insert into Statement
+    conn.execute("INSERT INTO Scores (Name,Stage,Time) VALUES('{name}','{stage}','{time}')".\
+            format(name=name, stage = Stage, time = Time))
+    conn.commit()
+   
+def Close():
+    #Close Statement
+    conn.close()
+    
+//Sample Run
 conn= sqlite3.connect('HighScores.sqlite')
-
-
-
-#Statement
-cursor = conn.execute("SELECT * from Scores ORDER BY Stage, Time")
-for row in cursor:
-    print("Name: " , row[1], " Stage: " , row[2], " Time: " , row[3])
-conn.close()
+Select()
+Insert()
+Close()
